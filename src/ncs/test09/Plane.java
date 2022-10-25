ed58675e3bbf63a038d2b315c9e744fc0b7b4d81
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-26 02:04:15 +0900

    20221026 | Update

---
+++ src/ncs/test09/Plane.java
@@ -0,0 +1,20 @@
+package ncs.test09;
+
+public class Plane {
+	String planeName;
+	int fuelSize;
+	
+	public Plane() {
+	
+	}
+
+	public Plane(String planeName, int fuelSize) {
+		
+		this.planeName = planeName;
+		this.fuelSize = fuelSize;
+	}
+	
+	public void int refuel(int fuel) {
+	
+	}
+}
